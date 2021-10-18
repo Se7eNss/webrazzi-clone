26dba78edca0299604d63a8e7409778a237f170a
Se7eNss <oguzhanseven18@gmail.com>
2021-10-18 21:29:00 +0300

    v2

--- src/components/home/SectionTwo.jsx
+++ src/components/home/SectionTwo.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import AllFrame from './frame11/AllFrame'
+import AllFrame19 from './frame19/AllFrame19'
 const SectionTwo = () => {
     return (
-        <div className="container">
+        <Fragment>
             <div className="row">
-                <AllFrame/>
+                <div className="headerEffect mt-3" style={{backgroundImage: ` url("/images/HeaderEffect.png")`}}>
+                    <h1 className="col-lg-8 offset-2 mt-4">Son Eklenenler</h1>
+                </div>  
             </div>
-            
-        </div>
+            <div className="container">
+                <div className="row allframe">
+                    <AllFrame/>
+                    <AllFrame19/>
+                </div>
+            </div>
+        </Fragment>
+       
     )
 }
 
---
+++ src/components/home/frame19/Frame19.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './frame19.css'
+
+const frame19 = () => {
+    return (
+        <div className="popular-wrapper col-lg-12 ">
+            <div className="baslik">Cem Yılmaz, NFT eserleri için  farklı 
+            bir pazarlama stratejisi belirledi </div>
+            <div className="writer">asdsa</div>
+        </div>
+    )
+}
+
+export default frame19
