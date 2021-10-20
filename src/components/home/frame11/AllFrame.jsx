07a67adf6ba45d52411130b96db58fb2cfb882cc
Se7eNss <oguzhanseven18@gmail.com>
2021-10-20 17:08:13 +0300

    v3

--- src/components/home/frame11/AllFrame.jsx
+++ src/components/home/frame11/AllFrame.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { Fragment, useEffect } from 'react'
 import Frame11 from './Frame11'
+
+import {getNews} from '../../../actions/newsActions';
+import { useDispatch, useSelector } from 'react-redux';
+
 const AllFrame = () => {
+    const dispatch =useDispatch();
+    const{news, error}=useSelector(state=>state.news)
+    useEffect(()=>{
+        if(error){
+            return alert.error(error)
+        }
+        dispatch(getNews())
+    },[])
     return (
-        <div className="col-lg-8 col-md-12">
-            <Frame11/>
-        </div>
+        <Fragment>
+                <div className="col-lg-8 col-md-12">
+                   {news.map(n =>{
+                        <Frame11 n={n}/>
+                   })}
+            
+                </div>
+        </Fragment>
     )
 }
 
---
+++ src/constants/newsConstant.js
@@ -0,0 +1,5 @@
+export const NEWS_REQUEST = 'NEWS_REQUEST'
+export const NEWS_SUCCESS = 'NEWS_SUCCESS'
+export const NEWS_FAIL = 'NEWS_FAIL'
+
+export const CLEAR_ERRORS = 'CLEAR_ERRORS'
